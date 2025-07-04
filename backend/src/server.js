5570ec3f4cbe2d1a517edc1c15d2c35a0e172886
Kevals15 <keval.shah1507@gmail.com>
2025-07-05 02:42:07 +0530

    Add notes package and update server configuration for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,33 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import noteRouter from './routes/notes.routes.js'
 import connectDb from './db/index.js'
-const app = express()
+import path from 'path'
 dotenv.config()
+const app = express()
+const __dirname = path.resolve()
+
+
+
+app.use(express.json())
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: process.env.CORS_ORIGIN
+    }))
+}
+
+app.use("/api/notes", noteRouter)
+
+
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+
+}
 connectDb()
     .then(() => {
         app.listen(process.env.PORT || 5000, () => {
@@ -14,9 +39,3 @@ connectDb()
     .catch((err) => {
         console.log(`DB CONNECTION FAILED ${err}`);
     })
-app.use(express.json())
-app.use(cors({
-    origin: process.env.CORS_ORIGIN
-}))
-
-app.use("/api/notes", noteRouter)
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api/notes" : "/api/notes"
 export const api = axios.create({
-    baseURL: "http://localhost:3000/api/notes"
+    baseURL: BASE_URL
 })
